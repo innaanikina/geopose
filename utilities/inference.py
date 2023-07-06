444e1c91dca66a8ae4dab62c31af38f13907f8d8
Inna Anikina <avril-avril@mail.ru>
2023-07-06 13:56:14 +0500

    fixed agl_pred indices

--- utilities/inference.py
+++ utilities/inference.py
@@ -300,7 +300,7 @@ def predict(args):
                     agl_pred = agl_pred[0, 0, :, :]
                     print(f"agl pred shape: {agl_pred.shape}")
                     agl_pred[agl_pred < 0] = 0
-                    res[:, i:i+tile_size, j:j+tile_size] = agl_pred[0, 0, :, :]
+                    res[:, i:i+tile_size, j:j+tile_size] = agl_pred[:, :]
                     print(f"agl sliced: {agl_pred[0, 0, :, :]}")
                     print(f"res: {res}")
                     j += tile_size
