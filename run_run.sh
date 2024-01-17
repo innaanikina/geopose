3bbe40029f04337c6ea73d9b0e8addc903b9611e
Alexandra Dunaeva <maryanova.av@yandex.ru>
2024-01-17 19:26:11 +0500

    adding run_run.sh

---
+++ run_run.sh
@@ -0,0 +1,11 @@
+export WANDB_DIR=/home/s0101/_scratch/wandb
+export WANDB_CACHE_DIR=/home/s0101/_scratch/wandb
+export WANDB_DATA_DIR=/home/s0101/_scratch/wandb
+export TORCH_HOME=/home/s0101/_scratch/torch
+export TRANSFORMERS_CACHE=/home/s0101/_scratch/transformers
+
+source /opt/intelpython3/bin/activate
+conda activate pytorch_k40_1.9
+module load gpu/cuda-11.5
+
+sbatch  -p v100 --mem=32000 --gres=gpu:v100:1 --cpus-per-task=1 -t 20:00:00 -x tesla-a100 --job-name=segm --output='./logs/test_cs %j.txt' --wrap='./run.sh'
